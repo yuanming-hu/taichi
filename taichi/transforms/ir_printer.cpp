dca6df4bee970547d2106aa249af2b4ef09d38dd
Yuanming Hu <yuanmhu@gmail.com>
2020-08-18 16:51:12 -0400

    update ir_printer

--- taichi/transforms/ir_printer.cpp
+++ taichi/transforms/ir_printer.cpp
@@ -476,7 +476,7 @@ class IRPrinter : public IRVisitor {
 
   void visit(OffloadedStmt *stmt) override {
     std::string details;
-    if (stmt->task_type == stmt->range_for) {
+    if (stmt->task_type == OffloadedStmt::TaskType::range_for) {
       std::string begin_str, end_str;
       if (stmt->const_begin) {
         begin_str = std::to_string(stmt->begin_value);
@@ -491,11 +491,13 @@ class IRPrinter : public IRVisitor {
       details =
           fmt::format("range_for({}, {}) grid_dim={} block_dim={}", begin_str,
                       end_str, stmt->grid_dim, stmt->block_dim);
-    } else if (stmt->task_type == stmt->struct_for) {
+    } else if (stmt->task_type == OffloadedStmt::TaskType::struct_for) {
       details = fmt::format(
           "struct_for({}) grid_dim={} block_dim={} bls={}",
           stmt->snode->get_node_type_name_hinted(), stmt->grid_dim,
           stmt->block_dim, scratch_pad_info(stmt->scratch_opt), stmt->bls_size);
+    } else if (stmt->task_type == OffloadedStmt::TaskType::serial) {
+      details = "serial";
     }
     if (stmt->task_type == OffloadedStmt::TaskType::listgen) {
       print("{} = offloaded listgen {}->{}", stmt->name(),
