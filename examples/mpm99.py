0490badd7dcacaa9a271d493e9c639356c35a5e2
Yuanming Hu <yuanmhu@gmail.com>
2020-08-23 12:08:53 -0400

    split

--- examples/mpm99.py
+++ examples/mpm99.py
@@ -19,23 +19,19 @@ Jp = ti.field(dtype=float, shape=n_particles) # plastic deformation
 grid_v = ti.Vector.field(2, dtype=float, shape=(n_grid, n_grid)) # grid node momentum/velocity
 grid_m = ti.field(dtype=float, shape=(n_grid, n_grid)) # grid node mass
 
+# Megakernel sync: 110ms
+# Megakernel async: 124ms
+
 @ti.kernel
 def substep():
   for i, j in grid_m:
     grid_v[i, j] = [0, 0]
     grid_m[i, j] = 0
   for p in x: # Particle state update and scatter to grid (P2G)
-    base = (x[p] * inv_dx - 0.5).cast(int)
-    fx = x[p] * inv_dx - base.cast(float)
-    # Quadratic kernels  [http://mpm.graphics   Eqn. 123, with x=fx, fx-1,fx-2]
-    w = [0.5 * (1.5 - fx) ** 2, 0.75 - (fx - 1) ** 2, 0.5 * (fx - 0.5) ** 2]
     F[p] = (ti.Matrix.identity(float, 2) + dt * C[p]) @ F[p] # deformation gradient update
-    h = ti.exp(10 * (1.0 - Jp[p])) # Hardening coefficient: snow gets harder when compressed
+  for p in x:
     if material[p] == 1: # jelly, make it softer
       h = 0.3
-    mu, la = mu_0 * h, lambda_0 * h
-    if material[p] == 0: # liquid
-      mu = 0.0
     U, sig, V = ti.svd(F[p])
     J = 1.0
     for d in ti.static(range(2)):
@@ -49,9 +45,19 @@ def substep():
       F[p] = ti.Matrix.identity(float, 2) * ti.sqrt(J)
     elif material[p] == 2:
       F[p] = U @ sig @ V.transpose() # Reconstruct elastic deformation gradient after plasticity
+    h = ti.exp(10 * (1.0 - Jp[p])) # Hardening coefficient: snow gets harder when compressed
+    mu, la = mu_0 * h, lambda_0 * h
+    if material[p] == 0: # liquid
+      mu = 0.0
+      
     stress = 2 * mu * (F[p] - U @ V.transpose()) @ F[p].transpose() + ti.Matrix.identity(float, 2) * la * J * (J - 1)
+    
     stress = (-dt * p_vol * 4 * inv_dx * inv_dx) * stress
     affine = stress + p_mass * C[p]
+    base = (x[p] * inv_dx - 0.5).cast(int)
+    fx = x[p] * inv_dx - base.cast(float)
+    # Quadratic kernels  [http://mpm.graphics   Eqn. 123, with x=fx, fx-1,fx-2]
+    w = [0.5 * (1.5 - fx) ** 2, 0.75 - (fx - 1) ** 2, 0.5 * (fx - 0.5) ** 2]
     for i, j in ti.static(ti.ndrange(3, 3)): # Loop over 3x3 grid node neighborhood
       offset = ti.Vector([i, j])
       dpos = (offset.cast(float) - fx) * dx
@@ -61,7 +67,11 @@ def substep():
   for i, j in grid_m:
     if grid_m[i, j] > 0: # No need for epsilon here
       grid_v[i, j] = (1 / grid_m[i, j]) * grid_v[i, j] # Momentum to velocity
+  for i, j in grid_m:
+    if grid_m[i, j] > 0: # No need for epsilon here
       grid_v[i, j][1] -= dt * 50 # gravity
+  for i, j in grid_m:
+    if grid_m[i, j] > 0: # No need for epsilon here
       if i < 3 and grid_v[i, j][0] < 0:          grid_v[i, j][0] = 0 # Boundary conditions
       if i > n_grid - 3 and grid_v[i, j][0] > 0: grid_v[i, j][0] = 0
       if j < 3 and grid_v[i, j][1] < 0:          grid_v[i, j][1] = 0
@@ -79,6 +89,7 @@ def substep():
       new_v += weight * g_v
       new_C += 4 * inv_dx * weight * g_v.outer_product(dpos)
     v[p], C[p] = new_v, new_C
+  for p in x:
     x[p] += dt * v[p] # advection
 
 group_size = n_particles // 3
@@ -97,7 +108,7 @@ while not gui.get_event(ti.GUI.ESCAPE, ti.GUI.EXIT):
   for s in range(int(2e-3 // dt)):
     substep()
   ti.sync()
-  print(f'{(time.time() - t) * 1000:.3f}')
+  print(f'{(time.time() - t) * 1000:.2f} ms')
   colors = np.array([0x068587, 0xED553B, 0xEEEEF0], dtype=np.uint32)
   gui.circles(x.to_numpy(), radius=1.5, color=colors[material.to_numpy()])
   gui.show() # Change to gui.show(f'{frame:06d}.png') to write images to disk
