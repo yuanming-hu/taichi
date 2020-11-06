93b92940341c5909c3ee496b4f824aced0c08f5a
Yuanming Hu <yuanmhu@gmail.com>
2020-11-05 22:52:18 -0500

    .

--- tests/python/test_bit_struct.py
+++ tests/python/test_bit_struct.py
@@ -4,12 +4,11 @@ import numpy as np
 
 def test_a():
     ti.init(arch=ti.cpu, debug=True, print_ir=True, cfg_optimization=False)
-    ci13 = ti.type_factory_.get_custom_int_type(13, True)
+    ci13 = ti.type_factory_.get_custom_int_type(14, True)
     return
-    cu19 = ti.type_factory_.get_custom_int_type(19, False)
 
 
 def test_b():
     ti.init(arch=ti.cpu, debug=True, print_ir=True, cfg_optimization=False)
-    ci13 = ti.type_factory_.get_custom_int_type(13, True)
+    ci13 = ti.type_factory_.get_custom_int_type(14, True)
     return
