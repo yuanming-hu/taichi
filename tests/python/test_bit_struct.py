cf2dcc1819eb7c93a3823ef62c97c22808c2665e
Yuanming Hu <yuanmhu@gmail.com>
2020-11-05 22:33:57 -0500

    reproduce

--- tests/python/test_bit_struct.py
+++ tests/python/test_bit_struct.py
@@ -2,17 +2,14 @@ import taichi as ti
 import numpy as np
 
 
-def test_simple_array():
+def test_a():
     ti.init(arch=ti.cpu, debug=True, print_ir=True, cfg_optimization=False)
     ci13 = ti.type_factory_.get_custom_int_type(13, True)
+    return
     cu19 = ti.type_factory_.get_custom_int_type(19, False)
 
-    return
 
-def test_custom_int_load_and_store():
+def test_b():
     ti.init(arch=ti.cpu, debug=True, print_ir=True, cfg_optimization=False)
     ci13 = ti.type_factory_.get_custom_int_type(13, True)
-    cu14 = ti.type_factory_.get_custom_int_type(14, False)
-    ci5 = ti.type_factory_.get_custom_int_type(5, True)
-
     return
