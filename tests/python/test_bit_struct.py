437e8e5434181e0f2317f733494c4323e4439c4c
Yuanming Hu <yuanmhu@gmail.com>
2020-11-05 22:32:12 -0500

    reproduce

--- tests/python/test_bit_struct.py
+++ tests/python/test_bit_struct.py
@@ -7,14 +7,7 @@ def test_simple_array():
     ci13 = ti.type_factory_.get_custom_int_type(13, True)
     cu19 = ti.type_factory_.get_custom_int_type(19, False)
 
-    x = ti.field(dtype=ci13)
-    y = ti.field(dtype=cu19)
-
-    N = 12
-
-    ti.root.dense(ti.i, N)._bit_struct(num_bits=32).place(x, y)
-
-    ti.get_runtime().materialize()
+    return
 
 def test_custom_int_load_and_store():
     ti.init(arch=ti.cpu, debug=True, print_ir=True, cfg_optimization=False)
